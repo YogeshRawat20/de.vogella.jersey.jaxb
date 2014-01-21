1a4c444301d69407cdf3c4efdc89baaa00683686
yogesh <yoyo.rawat@gmail.com>
2014-01-21 16:46:03 +0530

    First Cmmit

---
+++ src/de/vogella/jersey/jaxb/model/Todo.java
@@ -0,0 +1,18 @@
+package de.vogella.jersey.jaxb.model;
+
+public class Todo {
+	  private String summary;
+	  private String description;
+	  public String getSummary() {
+	    return summary;
+	  }
+	  public void setSummary(String summary) {
+	    this.summary = summary;
+	  }
+	  public String getDescription() {
+	    return description;
+	  }
+	  public void setDescription(String description) {
+	    this.description = description;
+	  }
+}
